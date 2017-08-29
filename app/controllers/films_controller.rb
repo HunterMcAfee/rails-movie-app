864ea891de41b5041c359a82f91f4b2f7698fa28
HunterMcAfee <huntmac10@gmail.com>
2017-08-29 16:26:48 -0400

    Finished CRUD for Films.  Added links between pages.

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,9 +3,9 @@ class FilmsController < ApplicationController
         @films = Film.all
     end
 
-    # def show
-    #     @film = Film.find(params[:id])
-    # end
+    def show
+        @film = Film.find(params[:id])
+    end
 
     def new
         @film = Film.new
@@ -15,12 +15,26 @@ class FilmsController < ApplicationController
         @film = Film.create!(film_params)
     end
 
-    # def edit
-    #     @film = Film.find(params[:id])
-    # end
+    def edit
+        @film = Film.find(params[:id])
+    end
+
+    def update
+        @film = Film.find(params[:id])
+        @film.update(film_params)
+
+        redirect_to film_path(@film)
+    end
+
+    def destroy
+        @film = Film.find(params[:id])
+        @film.destroy
+    
+        redirect_to films_path
+      end
 
     private
     def film_params
-       params.require(:film).permit(:title, :genere, :year, :synopsis, :picture) 
+       params.require(:film).permit(:title, :genre, :year, :synopsis, :picture) 
     end
 end
