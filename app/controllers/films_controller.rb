48654918cd7ef6b6a0b826e315f969ba7845fd2f
HunterMcAfee <huntmac10@gmail.com>
2017-08-29 15:34:40 -0400

    Added index routes that works and displays list of titles.  Create route works...created form partial

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,5 +1,26 @@
 class FilmsController < ApplicationController
-   def index
-       
-   end
+    def index
+        @films = Film.all
+    end
+
+    # def show
+    #     @film = Film.find(params[:id])
+    # end
+
+    def new
+        @film = Film.new
+    end
+
+    def create
+        @film = Film.create!(film_params)
+    end
+
+    # def edit
+    #     @film = Film.find(params[:id])
+    # end
+
+    private
+    def film_params
+       params.require(:film).permit(:title, :genere, :year, :synopsis, :picture) 
+    end
 end
